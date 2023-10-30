5490abb31dd58a86b21eb1bdb4a87551a613d1b0
Ryan Rahman <149414890+ryanrahman27@users.noreply.github.com>
2023-10-30 12:45:33 -0400

    Add files via upload

---
+++ NE111_Final_Game_Project.py
@@ -0,0 +1,42 @@
+import pygame
+
+pygame.init()
+
+
+#Creating Game Window
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("DBZ Fighter")
+
+#Load Background
+bg_image = pygame.image.load("bg_image - instasize.jpg").convert_alpha()
+
+#Function to Draw Background
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0,0))
+
+
+#Game Loop
+run = True
+while run:
+
+    #Draw Background
+    draw_bg()
+
+
+    #Event Handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    #Update Display
+    pygame.display.update()
+            
+
+#exit pygame
+
+pygame.quit()
