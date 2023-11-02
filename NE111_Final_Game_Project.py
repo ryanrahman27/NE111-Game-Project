b32351aa6015935ee0928e3afdc56b2a650aeefa
Lilgibbsy <owen@gibbsbunch.com>
2023-11-02 00:38:36 -0400

    Health bars fixed
    Reached 0:52:20 in the video. Next step is adding the sprites.

--- NE111_Final_Game_Project.py
+++ NE111_Final_Game_Project.py
@@ -32,7 +32,7 @@ def draw_bg():
 #function for drawing fighter health bars
 def draw_health_bar(health, x, y):
     ratio = health / 100
-    pygame.draw.rect(x - 2, y - 2, (404, 34))
+    pygame.draw.rect(screen, WHITE, (x -2, y -2, 404, 34))
     pygame.draw.rect(screen, RED, (x, y, 400, 30))
     pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
